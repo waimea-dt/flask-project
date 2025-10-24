3e5b555760b5b44ff3c5bcd8326d6a8f8a58a49d
Steve Copley <steve.copley@waimea.school.nz>
2025-10-24 15:32:14 +1300

    Tidied up module imports

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,7 @@
 from flask import Flask, render_template, flash, redirect, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import html
-
-from app.helpers.session import init_session
-from app.helpers.log     import init_logging
-from app.helpers.db      import init_database, connect_db
-from app.helpers.auth    import login_required
-from app.helpers.text    import init_text_filters
-from app.helpers.date    import init_date_filters
-from app.helpers.error   import init_error_handlers
+from app.helpers import *
 
 
 # Create the app
--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,1 +1,20 @@
-""" Helper module """
+"""Helper functions for Flask app"""
+
+from app.helpers.session import init_session
+from app.helpers.log import init_logging
+from app.helpers.db import init_database, connect_db
+from app.helpers.auth import login_required
+from app.helpers.text import init_text_filters
+from app.helpers.date import init_date_filters
+from app.helpers.error import init_error_handlers
+
+__all__ = [
+    'init_session',
+    'init_logging',
+    'init_database',
+    'connect_db',
+    'login_required',
+    'init_text_filters',
+    'init_date_filters',
+    'init_error_handlers',
+]
